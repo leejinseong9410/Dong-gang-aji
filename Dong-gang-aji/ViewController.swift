936198e28746069adda0f31627bc8123fba0b7f0
leejinseong9410 <81233734+leejinseong9410@users.noreply.github.com>
2021-11-09 19:27:05 +0900

    Initial Commit

---
+++ Dong-gang-aji/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dong-gang-aji
+//
+//  Created by MacBookPro on 2021/11/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
